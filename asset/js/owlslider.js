d32899efd97af35cda3c424deaa042e52c2f5b7e
iniyasin <iniyasin7@gmail.com>
2016-11-26 22:01:38 +0700

    perubahan file

---
+++ asset/js/owlslider.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+  $("#owl-slide").owlCarousel({
+ 
+      autoPlay: 20000, //Set AutoPlay to 3 seconds
+ 
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
---
+++ index.php
@@ -0,0 +1,210 @@
+<!--Bagian Header-->
+	<?php include("header.php"); ?>
+
+    <!--Ini Bagian Banner-->
+
+     <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="asset/pic/banner1.jpg" width="960px" height="500px" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>SELAMAT DATANG DI RUMAHKURSUS.COM</h1>
+              <p>Banyak Sekali Tempat Kursus Menanti Anda!</p>
+              <p><a class="btn btn-lg btn-primary" href="asset/login.html" role="button">Masuk</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="asset/pic/banner2.jpg" width="960px" height="500px" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>TEMUKAN KENYAMANAN DALAM BERKURSUS</h1>
+              <p>Ingin Kursus Tapi Ga Mau Keluar Rumah? Disi Tempatnya!</p>
+              <p><a class="btn btn-lg btn-primary" href="bantuan.html" role="button">Lebih Lanjut</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="asset/pic/banner3.jpg" width="960px" height="500px" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>BANYAK KATEGORI TERSEDIA</h1>
+              <p>Banyaknya Kategori Dan Tempat Kursus Membuat Anda Lebih Leluasa Dalam Mengembangkan Bakat</p>
+              <p><a class="btn btn-lg btn-primary" href="asset/kategori.html" role="button">Lihat Galeri</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <hr>
+
+    <!-- /.carousel -->
+    <!--Banner Selesai-->
+
+    <!--Bagian Pengenalan-->
+    
+<div class="container">
+<h1><b>Kenapa Harus RumahKursus?</b></h1>
+<div class="row">
+  <div class="col-sm-4">
+    <div class="thumbnail">
+      <img src="asset/pic/why.jpg" class="img-rounded" alt="Sub Kategori">
+      <div class="caption">
+        <h2>Lebih Mudah</h2>
+        <p>Anda bisa lebih mudah dalam mendapatkan tempat kursus yang anda inginkan!.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="thumbnail">
+      <img src="asset/pic/why.jpg" class="img-rounded" alt="Sub Kategori">
+      <div class="caption">
+        <h2>Lebih Nyaman</h2>
+        <p>Dengan sistem pembayaran yang kami buat,maka anda tidak perlu khawatir penipuan lagi.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-4">
+    <div class="thumbnail">
+      <img src="asset/pic/why.jpg" class="img-rounded" alt="Sub Kategori">
+      <div class="caption">
+        <h2>Lebih Terjamin</h2>
+        <p>Setelah menyelesaikan kursus yang diikuti,maka anda berhak mendapat sertifikat yang disahkan oleh pihak pengajar.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+    <hr>
+    <!--Pengenalan Selesai-->
+
+    <!--Siapa Yang Mengajari-->
+    <div class="container">
+      <div class="list-bawah"></div>
+        <h3>Siapa Saja Yang Mengajari?</h3>
+        <div class="fadeIn">
+            <div class="col-md-6 wow fadeInLeftBig">
+                <p class="table table-hover">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia libero at fermentum congue. Quisque fermentum tortor vel tellus laoreet dignissim in eu nisi. Maecenas bibendum gravida mi quis facilisis. Aenean ullamcorper tristique sodales. Duis tempor tincidunt arcu eget convallis. Sed ullamcorper, neque et fermentum malesuada, purus sem facilisis nunc, eget tincidunt metus augue scelerisque libero. Quisque interdum ultricies venenatis. Maecenas dui urna, malesuada eget nunc iaculis, egestas facilisis nibh. Curabitur vitae tristique arcu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse dapibus mollis enim sed blandit. Nunc vitae finibus augue. Nulla sit amet viverra lectus, ac semper risus. Nullam vel metus eu ante iaculis iaculis iaculis non justo.</p>
+            </div>
+            
+            <div class="col-md-6 pull-right wow fadeInRightBig">
+                <img class="fadeIn" src="./asset/pic/avatar.jpg" width="400px" height="350px">
+            </div>
+        </div>
+    </div>
+    <hr>
+    <!--Siapa Yang Mengajari Selesai-->
+
+    <!--Tempat Kursus Rekomendasi-->
+    <div class="container">
+      <h3>Tempat Kursus Rekomendasi</h3>
+      <div id="owl-slide" class="owl-carousel owl-theme">
+          <div class="cat-item">
+            <a href="#" style="text-decoration: none;color: grey;">
+              <div class="item">
+                <img src="asset/pic/logo1.png" alt="Owl Image">
+                  <div class="cap-item">
+					<hr>
+						<table class="table table-hover">
+							<p align = "left">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+						</table>
+						<input type="button" class="btn btn-success" value="Masuk Kelas Ini"></input>
+				  </div>
+               </div>
+            </a>  
+       </div>
+
+      <div class="cat-item">
+            <a href="#" style="text-decoration: none;color: grey;">
+              <div class="item">
+                <img src="asset/pic/logo2.png" alt="Owl Image">
+                  <div class="cap-item">
+					<hr>
+						<table class="table table-hover">
+							<p align = "left">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+						</table>
+						<input type="button" class="btn btn-success" value="Masuk Kelas Ini"></input>
+				  </div>
+              </div>
+            </a>  
+      </div>
+
+      <div class="cat-item">
+            <a href="#" style="text-decoration: none;color: grey;">
+              <div class="item">
+                <img src="asset/pic/logo3.jpg" alt="Owl Image">
+                  <div class="cap-item">
+					<hr>
+						<table class="table table-hover">
+							<p align = "left">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+						</table>
+						<input type="button" class="btn btn-success" value="Masuk Kelas Ini"></input>
+				  </div>
+              </div>
+            </a>  
+      </div>
+
+      <div class="cat-item">
+            <a href="#" style="text-decoration: none;color: grey;">
+              <div class="item">
+                <img src="asset/pic/logo4.jpg" alt="Owl Image">
+                  <div class="cap-item">
+					<hr>
+						<table class="table table-hover">
+							<p align = "left">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+						</table>
+						<input type="button" class="btn btn-success" value="Masuk Kelas Ini"></input>
+				  </div>
+              </div>
+            </a>  
+      </div>
+
+      <div class="cat-item">
+            <a href="#" style="text-decoration: none;color: grey;">
+              <div class="item">
+                <img src="asset/pic/logo.jpg" alt="Owl Image">
+                  <div class="cap-item">
+					<hr>
+						<table class="table table-hover">
+							<p align = "left">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+						</table>
+						<input type="button" class="btn btn-success" value="Masuk Kelas Ini"></input>
+				  </div>
+              </div>
+            </a>  
+      </div>
+    </div>
+    <div class="container">
+      <div class="list">
+        <a href="asset/kategori.html">Lihat Semua List</a>
+      </div>
+      </div>
+    </div>
+    <!--Tempat Kursus Rekomendasi Selesai-->
+
+	<!--Bagian Footer-->
+	<?php include("footer.php"); ?>
+
