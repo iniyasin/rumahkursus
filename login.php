ec0b8795a31cee7146e82685d3295d1aed9bb1ce
iniyasin <iniyasin7@gmail.com>
2016-12-10 20:31:30 +0700

    Halaman login dan CSS

--- login.php
+++ login.php
@@ -6,28 +6,104 @@
 		<link rel="stylesheet" href="asset/css/bootstrap/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" href="asset/css/bootstrap-social.css">
 		<link rel="stylesheet" href="asset/css/font-awesome/css/font-awesome.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="asset/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<div class="container">
-			<div class="container text-center">
-				<form class="form-signin" data-ember-action="2">
-					<h2 class="form-signin-heading">Masuk</h2>
-					<small class="text-muted">Koneksikan RUMAHKURSUS dengan sosial media anda!</small>
-					<br>
-					<small class="text-muted">atau masuk dengan akun RUMAHKURSUS</small>
-					<br><br>
-						<input id="ember360" class="ember-view ember-text-field form-control login-input" placeholder="Email Address" type="text">
-						<input id="ember361" class="ember-view ember-text-field form-control login-input-pass" placeholder="Password" type="password">
-						<script id="metamorph-22-start" type="text/x-placeholder"></script><script id="metamorph-22-end" type="text/x-placeholder"></script>
-						<button class="btn btn-lg btn-primary btn-block btn-center" type="submit" data-bindattr-3="3">Masuk</button>
-					<br>
-					<a href= "#" class="btn btn-block btn-social btn-facebook"><i class="fa fa-facebook"></i> Masuk Dengan Facebook</a>
-					<a href= "#" class="btn btn-block btn-social btn-google"><i class="fa fa-google"></i>Masuk Dengan Google+</a>
-					<br>
-				<small class="create-account text-muted">Tidak punya akun RUMAHKURSUS atau sosial media? 
-				<button id="ember363" class="ember-view btn btn-sm btn-default"> Daftar </button> </small>
-			 </form>
-			</div>
-		</div>
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel">Masuk atau Daftar</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-8" style="border-right: 1px dotted #C2C2C2;padding-right: 30px;">
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#Login" data-toggle="tab">Masuk</a></li>
+                            <li><a href="#Registration" data-toggle="tab">Daftar</a></li>
+                        </ul>
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="Login">
+                                <form role="form" class="form-horizontal">
+                                <div class="form-group">
+                                    <label for="email" class="col-sm-2 control-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="email1" placeholder="Email" />
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1" class="col-sm-2 control-label">Password</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-2">
+                                    </div>
+                                    <div class="col-sm-10">
+                                        <button type="submit" class="btn btn-primary btn-sm">Masuk</button>
+                                        <a href="javascript:;">Lupa Password?</a>
+                                    </div>
+                                </div>
+                                </form>
+                            </div>
+                            <div class="tab-pane" id="Registration">
+                                <form role="form" class="form-horizontal">
+                                <div class="form-group">
+                                    <label for="name" class="col-sm-2 control-label">Nama</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="name" placeholder="Nama" />
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="email" class="col-sm-2 control-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="email" placeholder="Email" />
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="mobile" class="col-sm-2 control-label">Handphone</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="mobile" placeholder="Handphone" />
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password" class="col-sm-2 control-label">Password</label>
+                                    <div class="col-sm-10">
+                                        <input type="password" class="form-control" id="password" placeholder="Password" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-2">
+                                    </div>
+                                    <div class="col-sm-10">
+                                        <button type="button" class="btn btn-primary btn-sm">Simpan & Lanjutkan</button>
+                                        <button type="button" class="btn btn-default btn-sm">Cancel</button>
+                                    </div>
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div id="OR" class="hidden-xs">ATAU</div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="row text-center sign-with">
+                            <div class="col-md-12">
+                                <h3>Masuk</h3>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="btn-group btn-group-justified">
+                                    <a href="#" class="btn btn-primary"><i class="fa fa-facebook"></i> Facebook</a> 
+                                    <a href="#" class="btn btn-danger"><i class="fa fa-google-plus"></i> Google</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 	</body>
 </html>
