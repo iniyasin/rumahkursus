75ff1cf6158a4edbf9e261f3faf995d6e940ebc8
Andika Wijaya Putra <andikawputra@gmail.com>
2016-12-11 19:58:06 +0700

    page about masih ancur

---
+++ about.php
@@ -0,0 +1,144 @@
+<!--Bagian Header-->
+	<?php include("header.php"); ?>
+
+    <!--Bagian Pengenalan-->
+    
+<div class="container">
+<br><br><br><br>
+<h1><b>Kami selalu berusaha untuk membantu meningkatkan<br> potensi dan keahlian yang kamu miliki.<br> Ayo berjuang bersama! </b></h1>
+</div>
+    <hr>
+    <!--Pengenalan Selesai-->
+
+    <!--Siapa Yang Mengajari-->
+    <div class="container">
+      <div class="list-bawah"></div>
+        
+        <div class="fadeIn">
+            <div class="col-md-6 wow fadeInLeftBig">
+              <h3>Tentang Kami</h3>
+                <p class="table table-hover">Rumahkursus adalah stratup yang memiliki fokus dalam bidang kursus. Pada jaman sekarang ini kita tahu bahwa perkembangan bisnis kursus sudah sangat pesat dan beragam jenis juga bidang yang diajarkannya.  
+                <br><br>
+                Nah, Di era yang sudah modern ini, kami tergerak untuk membantu dunia kursus untuk lebih berkembang lagi ke era digital. Bukan hanya mempermudah para pelaku bisnis kurus, tapi juga mempermudah calon - calon murid sekalian.</p>
+            </div>
+            
+            <div class="col-md-6 pull-right wow fadeInRightBig">
+              <h3>Misi Kami</h3>
+                <p class="table table-hover">Kami bukan startup seperti yang lainnya. kami tidak fokos hanya pada beberapa golongan tertentu. Kami berusaha untuk tumbuh dengan merangkul seluruh elemen mulai dari bawah sampai atas.  
+                <br><br>
+                kami ingin membantu dan mendorong masyarakat yang berada dalam golongan apapun(tua/muda, pria/wanita, kaya/miskin) untuk menemukan dan mengembangkan setiap potensi yang ada dalam dirinya.</p>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <!--Siapa Yang Mengajari Selesai-->
+
+    <!--Our Team-->
+
+    <section id="team" class="text-center section">
+  <div class="container">
+    <h2><span class="highlight_secondary">Tim</span> Kami</h2>
+
+    <h5>
+      Inilah orang -orang hebat yang ada dibalik rumahkursus.com.<br>Orang-orang yang memiliki semangat yang kuat untuk memajukan bangsa.
+    </h5>
+
+    <div class="hr big_size_hr invisible_hr">
+        <i class="fa fa-"></i>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <div class="team">
+          <div class="team-image">
+            <a href="#">
+              <img src="http://dummyimage.com/450x450/d9dadc/1c1f24.png&text=+" class="img-circle img-responsive">
+            </a>
+            <div class="team-links">
+              <div class="team-links-list">
+                <a href="#" target="_blank"><i class="fa fa-envelope"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-linked"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+              </div><!-- /.team-links-list -->
+            </div><!-- /.team-links -->
+          </div><!-- /.team-image -->
+          <div class="team-content">
+            <div class="team-name"><a href="#"><h5>Asep Berlian</h5></a></div>
+            <div class="team-role">Co-founder</div>
+            <div class="team-description"></div>
+          </div><!-- /.team-content -->
+        </div><!-- /.team -->
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="team">
+          <div class="team-image">
+            <a href="#">
+              <img src="http://dummyimage.com/450x450/d9dadc/1c1f24.png&text=+" class="img-circle img-responsive">
+            </a>
+            <div class="team-links">
+              <div class="team-links-list">
+                <a href="#" target="_blank"><i class="fa fa-envelope"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-linked"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+              </div><!-- /.team-links-list -->
+            </div><!-- /.team-links -->
+          </div><!-- /.team-image -->
+          <div class="team-content">
+            <div class="team-name"><a href="#"><h5>Dadang Konelo</h5></a></div>
+            <div class="team-role">Co-founder</div>
+            <div class="team-description"></div>
+          </div><!-- /.team-content -->
+        </div><!-- /.team -->
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="team">
+          <div class="team-image">
+            <a href="#">
+              <img src="http://dummyimage.com/450x450/d9dadc/1c1f24.png&text=+" class="img-circle img-responsive">
+            </a>
+            <div class="team-links">
+              <div class="team-links-list">
+                <a href="#" target="_blank"><i class="fa fa-envelope"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-linked"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+              </div><!-- /.team-links-list -->
+            </div><!-- /.team-links -->
+          </div><!-- /.team-image -->
+          <div class="team-content">
+            <div class="team-name"><a href="#"><h5>Mas Boy</h5></a></div>
+            <div class="team-role">Co-founder</div>
+            <div class="team-description"></div>
+          </div><!-- /.team-content -->
+        </div><!-- /.team -->
+      </div>
+    </div>
+
+    <div class="hr invisible_hr">
+        <i class="fa fa-"></i>
+    </div>
+
+    <div class="row">
+      <div class="col-md-2 col-sm-4 col-xs-6">
+
+    <div class="hr big_size_hr invisible_hr">
+        <i class="fa fa-"></i>
+    </div>
+
+  </div><!-- /.container -->
+</section>
+<!--Our Team Selesai-->
+
+    <!--Tempat Kursus Rekomendasi-->
+<div class="text-center section">
+<h3>Markas Kami</h3>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3961.255015917016!2d107.5317746!3d-6.8600097!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e464bc64c5a7%3A0x982cbc448d5b9ae5!2sJl.+Puri+Cipageran+Indah+I%2C+Cipageran%2C+Cimahi+Utara%2C+Kota+Cimahi%2C+Jawa+Barat+40511!5e0!3m2!1sid!2sid!4v1481460528863" width="720" height="360" frameborder="0" style="border:0" allowfullscreen></iframe>
+  <br><br>
+</div>
+    <!--Tempat Kursus Rekomendasi Selesai-->
+
+	<!--Bagian Footer-->
+	<?php include("footer.php"); ?>
+
